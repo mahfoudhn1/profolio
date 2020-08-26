d4a8e6c9d7f7ebd33cfb028e9b7d3d8ed5dba971
user <mahfoudhn99@gmail.com>
2020-08-26 22:47:39 +0100

    Updates

---
+++ precache-manifest.f97b1bac1a597f39c58edbb728071382.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387f3d65b3e975cad606cd2ac2925261",
+    "url": "/profolio/index.html"
+  },
+  {
+    "revision": "241e3a3f0bd73600719f",
+    "url": "/profolio/static/css/main.de15a7a8.chunk.css"
+  },
+  {
+    "revision": "7adf542bf3b8c2d09f65",
+    "url": "/profolio/static/js/2.1f4f10e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profolio/static/js/2.1f4f10e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241e3a3f0bd73600719f",
+    "url": "/profolio/static/js/main.009e8b2c.chunk.js"
+  },
+  {
+    "revision": "0b64f0e3854e326b461f",
+    "url": "/profolio/static/js/runtime-main.ed870991.js"
+  },
+  {
+    "revision": "971675ce9a677746e97cf1847a75be72",
+    "url": "/profolio/static/media/back.971675ce.jpg"
+  },
+  {
+    "revision": "2ec4617399a2f6c9dfd0e09618f9d28a",
+    "url": "/profolio/static/media/profile.2ec46173.jpeg"
+  }
+]);
